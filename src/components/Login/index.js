d07940f753604e8f7eadcf5c125c8056b99c6a22
Emanoel Queiroz <emanoelqueiroz@gmail.com>
2018-04-15 17:29:04 -0300

    Ajustes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -22,51 +22,52 @@ class Login extends Component {
 
         const { getFieldDecorator } = this.props.form;
         return (
-            <Row>
-                <Col span={9} />
-                <Col span={6}>
-                    <Form onSubmit={this.handleSubmit.bind(this)} className="login-form">
-                        <FormItem>
-                            {getFieldDecorator('userName', {
-                                rules: [{ required: true, message: 'Por favor insira seu nome de usuário!' }],
-                            })(
-                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Usuário" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: 'Por favor insira sua senha!' }],
-                            })(
-                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
-                            )}
-                        </FormItem>
-                        <FormItem>
-                            <Row>
-                                <Col span={12}>
-                                    {getFieldDecorator('remember', {
-                                        valuePropName: 'checked',
-                                        initialValue: true,
-                                    })(<Checkbox>Lembrar senha</Checkbox>)}
-                                </Col>
-                                <Col span={12}>
-                                    <a className="login-form-forgot" href="" style={{ float: 'right' }}>Esqueci minha senha</a>
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="login-form-button"
-                                    style={{ width: '100%' }}>
-                                    Log in
+            <div className="login-block">
+                <Row style={{ minWidth: '300px' }}>
+                    <Col span={24}>
+                    <h1 className="text-center">UltraBeats</h1>
+                        <Form onSubmit={this.handleSubmit.bind(this)}>
+                            <FormItem>
+                                {getFieldDecorator('userName', {
+                                    rules: [{ required: true, message: 'Por favor insira seu nome de usuário!' }],
+                                })(
+                                    <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Usuário" />
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                {getFieldDecorator('password', {
+                                    rules: [{ required: true, message: 'Por favor insira sua senha!' }],
+                                })(
+                                    <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Senha" />
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                <Row>
+                                    <Col span={12}>
+                                        {getFieldDecorator('remember', {
+                                            valuePropName: 'checked',
+                                            initialValue: true,
+                                        })(<Checkbox>Lembrar senha</Checkbox>)}
+                                    </Col>
+                                    <Col span={12}>
+                                        <a className="login-form-forgot" href="" style={{ float: 'right' }}>Esqueci minha senha</a>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Button
+                                        type="primary"
+                                        htmlType="submit"
+                                        className="login-form-button"
+                                        style={{ width: '100%' }}>
+                                        Log in
                                 </Button>
-                            </Row>
-                            Ou <a href="">registre agora!</a>
-                        </FormItem>
-                    </Form>
-                </Col>
-                <Col span={9} />
-            </Row>
+                                </Row>
+                                Ou <a href="">registre agora!</a>
+                            </FormItem>
+                        </Form>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
